55158788d97766ca978cc7bdf8057c66a0519379
Mateusz Gula <mateuszgua@wp.eu>
2023-01-17 22:37:36 +0100

    Feat: add save task, update task and delete task

--- app/views.py
+++ app/views.py
@@ -1,8 +1,55 @@
-from app import app
-from flask import jsonify
+from flask import jsonify, request
+from bson.objectid import ObjectId
 import socket
+
+from app import app
+from app import db
 
 @app.route("/")
 def index():
     hostname = socket.gethostname()
     return jsonify(message="Welcome to Tasks app! I am running inside {} pod!".format(hostname))
+
+@app.route("/taks")
+def get_all_tasks():
+    tasks = db.task.find()
+    data = []
+    for task in tasks:
+        item = {
+            "id": str(task["_id"]),
+            "task": task["task"]
+        }
+        data.append(item)
+    return jsonify(data=data)
+
+@app.route("/task", methods=["POST"])
+def create_task():
+    data = request.get_json(force=True)
+    db.task.insert_one({"task": data["task"]})
+    return jsonify(message="Task saved successfully!")
+
+@app.route("/task/<id>", methods=["PUT"])
+def update_task(id):
+    data = request.get_json(force=True)["task"]
+    response = db.task.update_one({"_id": ObjectId(id)}, {"$set":{"task":data}})
+    if response.matched_count:
+        message = "Task updated successfully!"
+    else:
+        message = "No task found!"
+    return jsonify(message=message)
+
+@app.route("/task/<id>", methods=["DELETE"])
+def delete_task(id):
+    response = db.task.delete_one({"_id":ObjectId(id)})
+    if response.deleted_count:
+        message = "Task deleted successfully!"
+    else:
+        message = "No task found!"
+    return jsonify(message=message)
+
+@app.route("/tasks/delete", methods=["POST"])
+def delete_all_tasks():
+    db.task.remove()
+    return jsonify(message="All Tasks deleted!")
+
+
