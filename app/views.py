0f16354340d48912ccb13c5eab150d931d9938ff
Mateusz Gula <mateuszgua@wp.eu>
2023-01-18 20:47:54 +0100

    Feat: add edit, delete one and delete all tasks

--- app/views.py
+++ app/views.py
@@ -6,10 +6,12 @@ from flask_pymongo import MongoClient
 from app import app
 from app import db
 
+
 @app.route("/")
 def index():
     hostname = socket.gethostname()
     return jsonify(message="Welcome to Tasks app! I am running inside {} pod!".format(hostname))
+
 
 @app.route("/tasks")
 def get_all_tasks():
@@ -17,6 +19,7 @@ def get_all_tasks():
     tasks = [task for task in _tasks]
 
     return render_template('index.html', tasks=tasks)
+
 
 @app.route("/add", methods=["POST"])
 def create_task():
@@ -31,28 +34,37 @@ def create_task():
     db.tododb.insert_one(task_doc)
     return redirect(url_for('get_all_tasks'))
 
+
 @app.route("/task/<id>", methods=["PUT"])
 def update_task(id):
     data = request.get_json(force=True)["task"]
-    response = db.task.update_one({"_id": ObjectId(id)}, {"$set":{"task":data}})
+    task_doc = {
+        'name': request.form["name"],
+        'description': request.form["description"],
+        'start_date': request.form["start_date"],
+        'end_date': request.form["end_date"],
+        'status': request.form["status"],
+        'priority': request.form["priority"],
+    }
+    response = db.tododb.update_one({"id": ObjectId(id)},
+                                    {"$set": task_doc})
     if response.matched_count:
         message = "Task updated successfully!"
     else:
         message = "No task found!"
-    return jsonify(message=message)
+    return render_template('panel.html', message=message)
+
 
 @app.route("/task/<id>", methods=["DELETE"])
 def delete_task(id):
-    response = db.task.delete_one({"_id":ObjectId(id)})
+    response = db.tododb.delete_one({"id": ObjectId(id)})
     if response.deleted_count:
         message = "Task deleted successfully!"
     else:
         message = "No task found!"
     return jsonify(message=message)
 
+
 @app.route("/tasks/delete", methods=["POST"])
 def delete_all_tasks():
-    db.task.remove()
-    return jsonify(message="All Tasks deleted!")
-
-
+    response = db.tododb.remove()
