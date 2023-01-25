ec544d9997623f45660799038f7e033615755816
Mateusz Gula <mateuszgua@wp.eu>
2023-01-25 16:27:32 +0100

    Feat: add errors handler in views

--- app/my_error.py
+++ app/my_error.py
@@ -3,29 +3,43 @@ class MyError(Exception):
     pass
 
 
-class UserNotExist(MyError):
-    """Raised when user is not exist in db """
-
-    def __str__(self):
-        return "User not exist in database."
-
-
-class TasksNotExist(MyError):
-    """Raised when tasks is not exist in db """
-
-    def __str__(self):
-        return "Task not exist in database."
-
-
-class DatabaseReaderProblem(MyError):
+class LoadTasksProblem(MyError):
     """Raised when is problem to load data from database """
 
     def __str__(self):
-        return "Problem to load data from database."
+        return "Problem to load tasks from database."
 
 
-class DatabaseWriterError(MyError):
+class LoadUserProblem(MyError):
+    """Raised when user is not exist in db """
+
+    def __str__(self):
+        return "Problem for load user for login. User not exist in database."
+
+
+class DatabaseWriteUserError(MyError):
     """Raised when is problem to save data in database """
 
     def __str__(self):
-        return "There is a problem to save data in database."
+        return "There is a problem to save user in database."
+
+
+class DatabaseWriteTaskError(MyError):
+    """Raised when is problem to save data in database """
+
+    def __str__(self):
+        return "There is a problem to save task in database."
+
+
+class DatabaseEditTaskError(MyError):
+    """Raised when is problem to edit data in database """
+
+    def __str__(self):
+        return "There is a problem to edit task in database."
+
+
+class DatabaseDeleteTaskError(MyError):
+    """Raised when is problem to delete data in database """
+
+    def __str__(self):
+        return "There is a problem to delete task in database."
