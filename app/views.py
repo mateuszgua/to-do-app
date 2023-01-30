5c45442929e75776ec04fb7fe46391ca56f9c0b0
Mateusz Gula <mateuszgua@wp.eu>
2023-01-30 22:44:56 +0100

    Feat: relocate run app for vercel v13

--- app/views.py
+++ app/views.py
@@ -20,7 +20,6 @@ def panel():
     try:
         if "user" not in session:
             is_user_login = None
-            # return redirect(url_for("login"))
             flash("Problem for load session...")
             return render_template("login.html")
         is_user_login = session["user"]
@@ -126,14 +125,11 @@ def create_task():
 @ app.route("/task/edit/<task_id>", methods=["POST"])
 def update_task(task_id):
     try:
-        select = request.form.get('status')
-        print(str(select))
         edit_name = request.form["name"]
         edit_description = request.form["description"]
         edit_start_date = request.form["start_date"]
         edit_end_date = request.form["end_date"]
         edit_status = request.form["status"]
-        # edit_status = request.form.get("status")
         edit_priority = request.form["priority"]
         response = collection_task.update_many({"id": ObjectId(task_id)},
                                                {"$set": {'name': edit_name,
