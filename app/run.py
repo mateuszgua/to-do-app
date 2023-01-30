495ad89ad28ae890f11394faa0bd03cf11674a6d
Mateusz Gula <mateuszgua@wp.eu>
2023-01-30 10:15:38 +0100

    Feat: relocate run app for vercel v1

--- app/run.py
+++
@@ -1,8 +0,0 @@
-from app import app, db, config
-
-if __name__ == '__main__':
-    with app.app_context():
-        app.config.from_object('app.config.DevelopmentConfig')
-        configuration = config.Config()
-    app.run(host=configuration.HOST, port=configuration.PORT,
-            debug=configuration.DEBUG)
